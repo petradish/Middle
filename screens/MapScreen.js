9e6d763aade2f20d1d01cc67dcc71f63c5e09b98
Petra Laohakul <petradish@gmail.com>
2019-09-26 15:02:48 -0400

    feat: added directions api to prefer subway route

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,64 +1,149 @@
 import React, {Component} from 'react';
-import { StyleSheet, View } from 'react-native';
-import MapView from 'react-native-maps';
-import MapViewDirections from 'react-native-maps-directions';
-import {GOOGLE_MAPS_APIKEY} from '../secrets';
+import { StyleSheet, View, Text, TextInput, TouchableHighlight, Keyboard } from 'react-native';
+import MapView, {Polyline, Marker} from 'react-native-maps';
+// import MapViewDirections from 'react-native-maps-directions';
+import PolyLine from '@mapbox/polyline'
+import {apiKey} from '../secrets';
+import _ from 'lodash'
 
 
-const origin = {latitude: 40.7305, longitude: -73.9091};
-const destination = {latitude: 40.705, longitude: -74.009};
+// const origin = {latitude: 40.7305, longitude: -73.9091};
+// const destination = {latitude: 40.705, longitude: -74.009};
 
-export default class LinksScreen extends Component {
+export default class MapScreen extends Component {
   constructor(){
     super();
     this.state = {
-      initialRegion: {
-        longitude: -73.9091,
-        latitude: 40.7305,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }
+      error: '',
+      latitude: 0,
+      longitude: 0,
+      destination: '',
+      pointCoords: [],
+      predictions: []
     };
-    this.onRegionChange = this.onRegionChange.bind(this)
-    this.getInitialState = this.getInitialState.bind(this)
+    this.onChangeDestinationDebounced = _.debounce(
+      this.onChangeDestination, 1000
+    )
   }
-  getInitialState() {
-    return {
-      initialRegion: {
-        longitude: -73.9091,
-        latitude: 40.7305,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+  
+  componentDidMount(){
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        });
       },
-    };
-  }
-  onRegionChange(region) {
-    this.setState({ region });
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={this.state.initialRegion}
-        onRegionChange={this.onRegionChange}
-        customMapStyle={mapStyle}
-        minZoomLevel={10} >
-          <MapViewDirections 
-          origin={origin}
-          destination={destination}
-          mode="TRANSIT"
-          optimizeWaypoints={true}
-          strokeWidth={2}
-          strokeColor="purple"
-          apikey={GOOGLE_MAPS_APIKEY} />
-      </MapView>
-      </View>
-
+      error => console.error(error), {enableHighAccuracy: true, maximumAge: 2000, timeout: 20000}
     );
   }
+  async getRouteDirections(destinationPlaceId, destinationName) {
+    try {
+      const response = await fetch(
+        `https://maps.googleapis.com/maps/api/directions/json?&mode=transit&origin=${
+          this.state.latitude
+        },${
+          this.state.longitude
+        }&destination=place_id:${destinationPlaceId}&departure_time=now&transit_mode=subway&key=${apiKey}`
+      );
+      const json = await response.json();
+      console.log(json);
+      const points = PolyLine.decode(json.routes[0].overview_polyline.points);
+      const pointCoords = points.map(point => {
+        return { latitude: point[0], longitude: point[1] };
+      });
+      this.setState({
+        pointCoords,
+        predictions: [],
+        destination: destinationName
+      });
+      Keyboard.dismiss();
+      this.map.fitToCoordinates(pointCoords);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  async onChangeDestination(destination) {
+    const apiUrl = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${apiKey}
+    &input=${destination}&location=${this.state.latitude},${
+      this.state.longitude
+    }&radius=2000`;
+    console.log(apiUrl);
+    try {
+      const result = await fetch(apiUrl);
+      const json = await result.json();
+      this.setState({
+        predictions: json.predictions
+      });
+      console.log(json);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  render() {
+    let marker = null;
+    if (this.state.pointCoords.length > 1){
+      marker = (
+        <Marker coordinate={this.state.pointCoords[this.state.pointCoords.length-1]} />
+      )
+    }
+    const predictions = this.state.predictions.map(prediction => (
+      <TouchableHighlight
+        onPress={() =>
+          this.getRouteDirections(
+            prediction.place_id,
+            prediction.structured_formatting.main_text
+          )
+        }
+        key={prediction.id}
+        > 
+          <Text style={styles.suggestions}>
+            {prediction.structured_formatting.main_text}
+          </Text>
+    
+      </TouchableHighlight>
+    ));
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        ref={map => {
+          this.map = map;
+        }}
+        style={styles.map}
+        region={{
+          latitude: this.state.latitude,
+          longitude: this.state.longitude,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121
+        }}
+        showsUserLocation={true}
+      >
+        <Polyline
+          coordinates={this.state.pointCoords}
+          strokeWidth={4}
+          strokeColor="red"
+        />
+        {marker}
+      </MapView>
+      <TextInput
+        placeholder="Enter destination..."
+        style={styles.destinationInput}
+        value={this.state.destination}
+        clearButtonMode="always"
+        onChangeText={destination => {
+          console.log(destination);
+          this.setState({ destination });
+          this.onChangeDestinationDebounced(destination);
+        }}
+      />
+      {predictions}
+    </View>
+  );
+  }
 }
-LinksScreen.navigationOptions = {
+
+MapScreen.navigationOptions = {
   title: 'Meet Me in the Middle!',
 };
 
@@ -347,11 +432,26 @@ const mapStyle = [
 ]
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    ...StyleSheet.absoluteFillObject
   },
   map: {
-    flex: 1
+    ...StyleSheet.absoluteFillObject
   },
+  destinationInput: {
+    height: 40,
+    borderWidth: 0.5,
+    marginTop: 50,
+    marginLeft: 5,
+    marginRight: 5,
+    padding: 5,
+    backgroundColor: "white"
+  },
+  suggestions: {
+    backgroundColor: "white",
+    padding: 5,
+    fontSize: 18,
+    borderWidth: 0.5,
+    marginLeft: 5,
+    marginRight: 5
+  }
 });

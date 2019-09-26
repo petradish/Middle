557c597572cd3b10c7958c9f2ff7f61941b1d061
Petra Laohakul <petradish@gmail.com>
2019-09-25 22:07:54 -0400

    fix: removed api

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -5,7 +5,7 @@ import MapViewDirections from 'react-native-maps-directions';
 
 const origin = {latitude: 37.3318456, longitude: -122.0296002};
 const destination = {latitude: 37.771707, longitude: -122.4053769};
-const GOOGLE_MAPS_APIKEY = 'AIzaSyDizIjGIW-3wH8Dnu4hBB9SUwgnbjT5ARw';
+// const GOOGLE_MAPS_APIKEY = 
 const mapStyle = [
   {
     "elementType": "geometry",
